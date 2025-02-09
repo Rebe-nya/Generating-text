5a76ce4579576a67f0b7a844f870744ac1590e7b
Rebe-nya <filip.slabinaf@gmail.com>
2025-02-09 23:00:14 +0100

    Fixes, text generator

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-function generateWord(numLetters) {
+function generateLetters(numLetters) {
     let word = "";
     let alphabet = "abcdefghijklmnopqrstuvwxyz";
-    let i = 0;
-    while (i<numLetters) {
+    let i = 0;  // Declare i
+    while (i < numLetters) {
         let characterOrder = Math.floor(Math.random() * alphabet.length);
         word += alphabet[characterOrder];
-//      word += characterOrder + " ";
         i++;
     }
     return word;
 }
 
-function generateSentence(numWords) {
-    let sentence = ""
+function generateWords(numWords) {
+    let sentence = "";
     let i = 0;
-    while (i<numWords) {
-        sentence += generateWord(5);
+    while (i < numWords) {
+        sentence += generateLetters(Math.floor(Math.random() * (7 - 2 + 1)) + 2) + " ";
+        i++;
     }
-    return sentence;
+    sentence = sentence.trim() + ". "; // Remove trailing space and add period
+    return sentence.charAt(0).toUpperCase() + sentence.slice(1); // Capitalize first letter
 }
 
-console.log(generateSentence(5));
+function generateSentences(numSentences) {
+    let text = "";
+    let = i = 0;
+    while (i < numSentences) {
+        text += generateWords(Math.floor(Math.random() * (10 - 5 + 1)) + 5);
+        i++;
+    }
+    return text
+}
+
+const textGenerator = document.getElementById("textGenerator");
+textGenerator.innerHTML = generateSentences(Math.floor(Math.random() * (15 - 7 + 1)) + 7);
